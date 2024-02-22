8c1e4ede59b2615aa2de79b454a09090251134fb
Zhikang Niu <73390819+ZhikangNiu@users.noreply.github.com>
2024-02-22 11:06:49 +0800

    Update download_fsd50k.sh

--- download_fsd50k.sh
+++ download_fsd50k.sh
@@ -19,25 +19,11 @@ PREFIX_URL="https://zenodo.org/record/4060432/files"
 
 OUTPUT_PATH="/data/v-zhikangniu/FSD50K/"
 
-# mkdir -p $OUTPUT_PATH/{clean_fullband,noise_fullband}
-
 for DATA in ${FSD50K_NAMES[@]}
 do
     URL="$PREFIX_URL/$DATA?download=1"
     echo "Download: $URL"
 
-    # DRY RUN: print HTTP response and Content-Length
-    # WITHOUT downloading the files
-    # curl -s -I "$URL" | head -n 2
-
-    # Actually download the files: UNCOMMENT when ready to download
-    # curl "$URL" -o "$OUTPUT_PATH/$BLOB"
-
-    # Same as above, but using wget
-    # wget "$URL" -O "$OUTPUT_PATH/$BLOB"
-
-    # Same, + unpack files on the fly
-    # curl "$URL" | tar -C "$OUTPUT_PATH" -f - -x -j
     aria2c -s16 -x16 "$URL" -d "$OUTPUT_PATH"
 done
 
