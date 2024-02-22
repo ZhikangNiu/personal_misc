1ec4abf4ef83e9f91d4d13b50cb3d41e4409ab6d
Zhikang Niu <73390819+ZhikangNiu@users.noreply.github.com>
2024-02-22 11:08:33 +0800

    Update download_musdb18.sh

--- download_musdb18.sh
+++ download_musdb18.sh
@@ -7,25 +7,11 @@ PREFIX_URL="https://zenodo.org/records/1117372/files/"
 
 OUTPUT_PATH="/data/v-zhikangniu/MUSDB18/"
 
-# mkdir -p $OUTPUT_PATH/{clean_fullband,noise_fullband}
-
 for DATA in ${DATA_NAMES[@]}
 do
     URL="$PREFIX_URL/$DATA?download=1"
     echo "Download: $URL"
 
-    # DRY RUN: print HTTP response and Content-Length
-    # WITHOUT downloading the files
-    # curl -s -I "$URL" | head -n 2
-
-    # Actually download the files: UNCOMMENT when ready to download
-    # curl "$URL" -o "$OUTPUT_PATH/$BLOB"
-
-    # Same as above, but using wget
-    # wget "$URL" -O "$OUTPUT_PATH/$BLOB"
-
-    # Same, + unpack files on the fly
-    # curl "$URL" | tar -C "$OUTPUT_PATH" -f - -x -j
     aria2c -s16 -x16 "$URL" -d "$OUTPUT_PATH"
 done
 
