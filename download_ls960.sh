7d734533519f2d78e526815ba80eb8ddf4dfd9b9
Zhikang Niu <73390819+ZhikangNiu@users.noreply.github.com>
2024-02-22 11:05:45 +0800

    Update download_ls960.sh

--- download_ls960.sh
+++ download_ls960.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
 
-
 DATA_NAMES=(
     train-clean-100.tar.gz
     train-clean-360.tar.gz
@@ -12,29 +11,21 @@ DATA_NAMES=(
     )
 PREFIX_URL="https://www.openslr.org/resources/12/"
 
-OUTPUT_PATH="/scratch/v-zhikangniu/"
+OUTPUT_PATH="/home/v-zhikangniu/descript-audio-codec/data"  
 
 cd $OUTPUT_PATH
-# mkdir -p $OUTPUT_PATH/{clean_fullband,noise_fullband}
 
-for DATA in ${DATA_NAMES[@]}
-do
-    URL="$PREFIX_URL/$DATA"
-    echo "Download: $URL"
+for DATA in ${DATA_NAMES[@]}  
+do  
+    URL="${PREFIX_URL}${DATA}"  
+    echo "Download: $URL"  
+  
+    # Download the files using aria2c  
+    aria2c -s16 -x16 "$URL" -d "$OUTPUT_PATH"  
+      
+    # Unpack files  
+    tar -xvf $DATA -C $OUTPUT_PATH  
 
-    # DRY RUN: print HTTP response and Content-Length
-    # WITHOUT downloading the files
-    # curl -s -I "$URL" | head -n 2
-
-    # Actually download the files: UNCOMMENT when ready to download
-    # curl "$URL" -o "$OUTPUT_PATH/$BLOB"
-
-    # Same as above, but using wget
-    # wget "$URL" -O "$OUTPUT_PATH/$BLOB"
-
-    # Same, + unpack files on the fly
-    # curl "$URL" | tar -C "$OUTPUT_PATH" -f - -x -j
-    aria2c -s16 -x16 "$URL" -d "$OUTPUT_PATH"
-    
-    tar --use-compress-program="pigz" -xvpf $DATA
-done
+    # Delete files
+    # tar -xvf $DATA -C $OUTPUT_PATH && rm $DATA
+done  
